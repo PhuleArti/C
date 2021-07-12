970cf7aa338d118c28109d684b5ade4211cd501d
PhuleArti <77423884+PhuleArti@users.noreply.github.com>
2021-07-12 11:04:55 +0530

    Program to print 5 to 1 on screan.

---
+++ 1.3.c
@@ -0,0 +1,22 @@
+/*problem statement:
+                                     Program to print 5 to 1 Numbers on screen.
+*/
+#include<stdio.h>
+
+void Display()
+{
+      int i=0;
+      
+      for(i=5;i>=1;i--)
+      {
+            printf("%d\t",i);
+      }
+}
+int main()
+{
+       Display();
+       
+       return 0;
+}
+
+
